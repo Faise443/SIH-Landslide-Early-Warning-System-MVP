1d34f817447f909ca768330c52f79192a744c5f1
Faise443 <YOUR_GITHUB_EMAIL>
2026-08-30 15:28:01 +0530

    Fix React build

--- frontend/src/MapDashboard.js
+++ frontend/src/MapDashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, CircleMarker, Popup, useMapEvents } from 'react-leaflet';
 import axios from 'axios';
